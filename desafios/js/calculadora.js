5309882f990c330da29098ec55914509a8c6ef14
JulianaNishimura <juliana.nishimura.tds24@gmail.com>
2025-03-11 17:18:02 -0300

    array

--- desafios/js/calculadora.js
+++ desafios/js/calculadora.js
@@ -2,6 +2,7 @@ const processar = (digito) => {
   let digitovalorEle = document.getElementById('digito');
   if (digitovalorEle instanceof HTMLInputElement) {
     let digitoValor = parseInt(digitovalorEle.value);
+    
     let telaEle = document.getElementById('exibirNumero');
     if (telaEle instanceof HTMLDivElement) {
       let tela = telaEle.querySelector('span');
