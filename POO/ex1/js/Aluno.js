5958577c053fc9e09dc4f3edb7dfd04039f87675
JulianaNishimura <juliana.nishimura.tds24@gmail.com>
2025-04-01 13:46:19 -0300

    aa

--- POO/ex1/js/Aluno.js
+++ POO/ex1/js/Aluno.js
@@ -1,4 +1,4 @@
-class Aluno{
+export default class Aluno{
     #nome;
     #notas = [];
 
@@ -27,6 +27,16 @@ class Aluno{
         }
     }
 
+    // Método para adicionar uma nota individualmente
+    adicionarNota(novaNota) {
+    //prettier-ignore
+        if ((novaNota >= 0) && (novaNota <= 10)) {
+            this.#notas.push(novaNota);
+        } else {
+            alert('Nota inválida. Deve ser um número entre 0 e 10.');
+        }
+    }
+
     #validarNotas(notas){
         let notasValidas = notas.every((value) => {return value <= 10 && value >= 0});
         if(!notasValidas){
@@ -38,8 +48,13 @@ class Aluno{
     }
 
     calcularMedia(notas){
+        if (!this.#validarNotas(this.#notas)) {
+            return 0; // Não pode calcular a média se as notas não forem válidas
+        }
+
         if(this.#notas.length === 0){
             alert("Não é possível dividir por 0.")
+            return 0;
         } else {
             let somaDeNotas = notas.reduce((acumulador,atual) => acumulador + atual,0);
             return (somaDeNotas/this.#notas.length).toFixed(2);
