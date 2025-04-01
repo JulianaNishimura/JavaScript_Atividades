56d52075343b01d3381223ddb9e79deae57d14cc
JulianaNishimura <juliana.nishimura.tds24@gmail.com>
2025-04-01 16:58:34 -0300

    poo

---
+++ POO/ex3/js/Pedido.js
@@ -0,0 +1,16 @@
+export default class Pedido{
+    #itens = [];
+
+    constructor(itens = []){
+        this.#itens = itens;
+    }
+
+    get itens(){
+        return this.#itens;
+    }
+
+    set itens(itens){
+        this.#itens = itens;
+    }
+
+}
